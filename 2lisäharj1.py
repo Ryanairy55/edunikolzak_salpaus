7d4340a3c091c9e7a00e03845c4aaf5376dc35b5
ptchtrns <62998707+Ryanairy55@users.noreply.github.com>
2023-08-17 13:26:45 +0300

    Create 2lisäharj1.py

---
+++ 2lisäharj1.py
@@ -0,0 +1,10 @@
+print("Anna lause: ")
+lause = str(input())
+raja1 = lause.find(" ")
+raja2 = raja1 + 1
+while True:
+    if lause[raja2] == " ":
+        break
+    else:
+        raja2 += 1
+print(lause[raja1:raja2])
