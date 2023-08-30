88fa3b2c860ce729162b65195b7fc9f227a53a77
ptchtrns <62998707+Ryanairy55@users.noreply.github.com>
2023-08-30 20:40:22 +0300

    Create harjoitus3.py

---
+++ harjoitus3.py
@@ -0,0 +1,16 @@
+bl = str(input("Anna 4-bittinen binääriluku: "))
+luku = 0
+if len(bl) != 4:
+    print("Syötteesi ei ollut nelimerkkinen.")
+else:
+    i = 3
+    a = 1
+    while i != -1:
+        if (bl[i] != "1") and (bl[i] != "0"):
+            print("Luku ei ole binääri")
+            break
+        else:
+            luku += a * int(bl[i])
+            a *= 2
+            i -= 1
+print(luku)
