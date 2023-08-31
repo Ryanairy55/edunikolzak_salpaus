7f1786f53bc0a6dd7370a4e845792116c6aaa1be
ptchtrns <62998707+Ryanairy55@users.noreply.github.com>
2023-08-31 13:26:04 +0300

    Update lisäharjoitus2_3.py

--- lisäharjoitus2_3.py
+++ lisäharjoitus2_3.py
@@ -1,3 +1,5 @@
+# Tämä ohjelma muuttaa syötteenään saamansa merkkijonon
+# kaikki kirjaimet isoiksi kirjaimiksi
 mj = input("Anna lause: ")
 iso = input("Anna sana: ")
 print(mj.replace(iso, iso.upper()))
