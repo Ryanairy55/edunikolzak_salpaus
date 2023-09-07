b45b961f77ae244729f6442e3482e4e78eca7917
ptchtrns <62998707+Ryanairy55@users.noreply.github.com>
2023-09-07 10:15:16 +0300

    Update lisäharjoitus4_3.py

--- lisäharjoitus4_3.py
+++ lisäharjoitus4_3.py
@@ -3,7 +3,7 @@ mjono2 = str(input("Anna toka sana: "))
 onkoanagrammi = True # tämä boolean muuttuja on tehty, jotta voi kertoa ovatko sanat ovat anagrammit ohjelman lopussa
 if len(mjono1) != len(mjono2):
     onkoanagrammi = False
-for i in range(0, len(mjono1)-1):
+for i in range(0, len(mjono1)):
     if mjono1.count(mjono1[i]) != mjono2.count(mjono1[i]):
         onkoanagrammi = False
         break # break jos kirjainten määrä erottaa
