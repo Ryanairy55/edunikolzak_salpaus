8d58ecaf9a1763b7152c6abca645a926ea23f33d
ptchtrns <62998707+Ryanairy55@users.noreply.github.com>
2023-09-07 10:15:30 +0300

    Update lisäharjoitus4_2.py

--- lisäharjoitus4_2.py
+++ lisäharjoitus4_2.py
@@ -1,5 +1,5 @@
 mj = str(input("Anna merkkijono: "))
 print("Isot kirjaimet:")
-for i in range(0, len(mj)-1): # for joka merkkijonon kirjain
+for i in range(0, len(mj)): # for joka merkkijonon kirjain
     if mj[i].isupper(): # jos kirjain on iso
         print(mj[i]) # tulosta sen
